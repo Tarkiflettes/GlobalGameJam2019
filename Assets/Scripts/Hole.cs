2a0f1325d3986f7fe1d7bcd13f18c842cf22d440
Lionel Jouin <lionel.jouin@outlook.com>
2019-01-26 16:57:08 +0100

    Platform | hole

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.Player;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Hole : MonoBehaviour
+    {
+
+        private void Start()
+        {
+        
+        }
+        
+        private void Update()
+        {
+        
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var playerController = collision.gameObject.GetComponent<PlayerController>();
+            if (playerController == null)
+                return;
+            if (SameType(playerController) == false)
+                return;
+            OpenForPlayer(playerController);
+        }
+
+        private void OpenForPlayer(PlayerController playerController)
+        {
+        }
+
+        private bool SameType(PlayerController playerController)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Platform : MonoBehaviour
+    {
+
+        private List<Hole> holes = new List<Hole>();
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+    }
+}
