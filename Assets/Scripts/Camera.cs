d9718ed7ed9db4773a0282ddc27044e644cc0e38
Lionel Jouin <lionel.jouin@outlook.com>
2019-01-27 02:20:45 +0100

    Camera follows player

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,25 +1,24 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Player;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class Camera : MonoBehaviour
     {
+        
+        public float Offset = 3.5f;
 
-        public float Speed = 100;
+        private PlayerController _playerController;
 
         private void Start()
         {
-
+            _playerController = FindObjectOfType<PlayerController>();
         }
 
         private void Update()
         {
-
+            transform.position = new Vector3(transform.position.x, _playerController.gameObject.transform.position.y + Offset, transform.position.z);
         }
-
-        public void GoDown()
-        {
-            transform.position += Vector3.forward * Time.deltaTime * Speed;
-        }
+        
     }
 }
